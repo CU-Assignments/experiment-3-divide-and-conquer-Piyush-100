2177e3723b20117c367fb90be0184f5b99f46283
Piyush-100 <157004163+Piyush-100@users.noreply.github.com>
2025-02-14 15:58:56 +0530

    Add files via upload

---
+++ TreeNode.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution { 
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            result.add(curr.val);
+            curr = curr.right;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        Solution obj = new Solution();
+        System.out.println(obj.inorderTraversal(root)); 
+    }
+}
+
